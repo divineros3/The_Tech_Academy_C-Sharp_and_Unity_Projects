b6ef09959e3a894fafbd28e1c2329424539ebdd8
divineros3 <divineros3@icloud.com>
2023-05-09 12:04:26 -0600

    Boolean Logic Submission Assignment

---
+++ BooleanLogicSubmission/BooleanLogicSubmission/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicSubmission
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? (answer with \"true\" or \"false\")");
+            string DUI = Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age >= 15 && DUI == "false" && tickets <= 3);
+            Console.ReadLine();
+
+        }
+    }
+}
