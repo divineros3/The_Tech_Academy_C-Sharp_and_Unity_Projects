82b9e6242f32fdef456fccff6449dfef86c71f0e
divineros3 <divineros3@icloud.com>
2023-05-11 12:19:02 -0600

    Console App Submission Update

--- ConsoleAppSubmission/ConsoleAppSubmission/Program.cs
+++ ConsoleAppSubmission/ConsoleAppSubmission/Program.cs
@@ -10,6 +10,46 @@ namespace ConsoleAppSubmission
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Guess a number between 0 and 50.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 3;
+
+            do
+            {
+                while (!isGuessed)
+                {
+                    switch (number)
+                    {
+                        case 46:
+                            Console.WriteLine("You guessed 46. Try again.");
+                            Console.WriteLine("Guess a number?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 21:
+                            Console.WriteLine("You guessed 21. Try again.");
+                            Console.WriteLine("Guess a number?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 10:
+                            Console.WriteLine("You guessed 10. Try again.");
+                            Console.WriteLine("Guess a number?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 3:
+                            Console.WriteLine("You guessed 3. That is correct!");
+                            isGuessed = true;
+                            break;
+                        default:
+                            Console.WriteLine("That is incorrect.");
+                            Console.WriteLine("Guess a number?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                    }
+                }
+            }
+            while (isGuessed);
+
+            Console.ReadLine();
         }
     }
 }
