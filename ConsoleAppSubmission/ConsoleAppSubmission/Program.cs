5f61d7053f22216b6e08fe3dc92ba22a04a9569f
divineros3 <divineros3@icloud.com>
2023-05-11 15:18:51 -0600

    Console App Submission Update

--- ConsoleAppSubmission/ConsoleAppSubmission/Program.cs
+++ ConsoleAppSubmission/ConsoleAppSubmission/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleAppSubmission
 {
@@ -16,38 +12,35 @@ namespace ConsoleAppSubmission
 
             do
             {
-                while (!isGuessed)
+                switch (number)
                 {
-                    switch (number)
-                    {
-                        case 46:
-                            Console.WriteLine("You guessed 46. Try again.");
-                            Console.WriteLine("Guess a number?");
-                            number = Convert.ToInt32(Console.ReadLine());
-                            break;
-                        case 21:
-                            Console.WriteLine("You guessed 21. Try again.");
-                            Console.WriteLine("Guess a number?");
-                            number = Convert.ToInt32(Console.ReadLine());
-                            break;
-                        case 10:
-                            Console.WriteLine("You guessed 10. Try again.");
-                            Console.WriteLine("Guess a number?");
-                            number = Convert.ToInt32(Console.ReadLine());
-                            break;
-                        case 3:
-                            Console.WriteLine("You guessed 3. That is correct!");
-                            isGuessed = true;
-                            break;
-                        default:
-                            Console.WriteLine("That is incorrect.");
-                            Console.WriteLine("Guess a number?");
-                            number = Convert.ToInt32(Console.ReadLine());
-                            break;
-                    }
+                    case 46:
+                        Console.WriteLine("You guessed 46. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 21:
+                        Console.WriteLine("You guessed 21. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 10:
+                        Console.WriteLine("You guessed 10. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("You guessed 3. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("That is incorrect.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
                 }
             }
-            while (isGuessed);
+            while (!isGuessed);
 
             Console.ReadLine();
         }
