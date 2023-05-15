5f5f446164629db619a829ff3edf1a1fc92c73b4
divineros3 <divineros3@icloud.com>
2023-05-15 11:45:06 -0600

    Console App Submission Update

--- ConsoleAppSubmission/ConsoleAppSubmission/Program.cs
+++ ConsoleAppSubmission/ConsoleAppSubmission/Program.cs
@@ -43,6 +43,45 @@ namespace ConsoleAppSubmission
             while (!isGuessed);
 
             Console.ReadLine();
+
+
+            Console.WriteLine("Guess a number between 0 and 50.");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed1 = number1 == 7;
+
+            while (!isGuessed1)
+            {
+                switch (number1)
+                {
+                    case 46:
+                        Console.WriteLine("You guessed 46. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number1 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 21:
+                        Console.WriteLine("You guessed 21. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number1 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 10:
+                        Console.WriteLine("You guessed 10. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number1 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("You guessed 7. That is correct!");
+                        isGuessed1 = true;
+                        break;
+                    default:
+                        Console.WriteLine("That is incorrect.");
+                        Console.WriteLine("Guess a number?");
+                        number1 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
