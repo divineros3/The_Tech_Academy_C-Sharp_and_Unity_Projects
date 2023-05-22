49316c17ccc4282e1c81a03bc9de3367abbfe400
divineros3 <divineros3@icloud.com>
2023-05-22 15:13:27 -0600

    Arrays Submission Assignment

---
+++ ArraysSubmission/ArraysSubmission/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ArraysSubmission
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> inList = new List<string>();
+            inList.Add("Hello");
+            inList.Add("My name is Divine");
+            inList.Add("My favorite color is pink");
+
+            Console.WriteLine("Please select an index number.");
+            int ListIndex = Convert.ToInt32(Console.ReadLine());
+            if (ListIndex < 0 || ListIndex > inList.Count - 1)
+            {
+                Console.WriteLine("Index does not exist.");
+            }
+            else
+            {
+                Console.WriteLine(inList[ListIndex]);
+            }
+            Console.ReadLine();
+
+            int[] numArray = new int[] { 1, 3, 5, 7, 9, 0, 2, 4, 6, 8 };
+
+            Console.WriteLine("Please select an index number.");
+            int numIndex = Convert.ToInt32(Console.ReadLine());
+            if(numIndex < 0 || numIndex > numArray.Length - 1)
+            {
+                Console.WriteLine("Index does not exist.");
+            }
+            else
+            {
+                Console.WriteLine(numArray[numIndex]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
