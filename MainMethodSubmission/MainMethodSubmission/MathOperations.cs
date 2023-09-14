3743363e4b5c0b88866d621307cb9ad49828d62b
divineros3 <divineros3@icloud.com>
2023-09-14 12:58:55 -0600

    Main Method Submission Assignment

---
+++ MainMethodSubmission/MainMethodSubmission/MathOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    public class MathOperations
+    {
+        public int Calculate(int number)
+        {
+            return number + 10;
+        }
+
+        public int Calculate(decimal number)
+        {
+            return (int)(number * 2);
+        }
+
+        public int Calculate(string input)
+        {
+            if (int.TryParse(input, out int number))
+            {
+                return number - 5;
+            }
+            else
+            {
+                throw new ArgumentException("Input is not a valid integer.");
+            }
+        }
+    }
+}
---
+++ MainMethodSubmission/MainMethodSubmission/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class Program
+    {
+        static void Main()
+        {
+            MathOperations mathOperations = new MathOperations();
+
+            int result1 = mathOperations.Calculate(20);
+            Console.WriteLine($"Result for integer input: {result1}");
+
+            decimal decimalInput = 7.5M;
+            int result2 = mathOperations.Calculate(decimalInput);
+            Console.WriteLine($"Result for decimal input {result2}");
+
+            string stringInput = "15";
+            int result3 = mathOperations.Calculate(stringInput);
+            Console.WriteLine($"Result for string input: {result3}");
+
+            Console.ReadLine();
+        }
+    }
+}
