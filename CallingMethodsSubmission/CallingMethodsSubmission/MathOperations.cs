6f2b3cee95464c33b78d049e595454dc265a86ef
divineros3 <divineros3@icloud.com>
2023-09-14 12:13:23 -0600

    Calling Methods Submission Assignment

---
+++ CallingMethodsSubmission/CallingMethodsSubmission/MathOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    public class MathOperations
+    {
+        public int AddFive(int number)
+        {
+            return number + 5;
+        }
+
+        public int Square(int number)
+        {
+            return number * number;
+        }
+        public int Double(int number)
+        {
+            return number * 2;
+        }
+
+    }
+}
---
+++ CallingMethodsSubmission/CallingMethodsSubmission/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    class Program
+    {
+        static void Main()
+        {
+            MathOperations mathOperations = new MathOperations();
+
+            Console.Write("Enter an integer: ");
+            if (int.TryParse(Console.ReadLine(), out int userInput))
+            {
+                int result1 = mathOperations.AddFive(userInput);
+                int result2 = mathOperations.Square(userInput);
+                int result3 = mathOperations.Double(userInput);
+
+                Console.WriteLine($"Result of adding 5: {result1}");
+                Console.WriteLine($"Result of squaring: {result2}");
+                Console.WriteLine($"Result of doubling: {result3}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
