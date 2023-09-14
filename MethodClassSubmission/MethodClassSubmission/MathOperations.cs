37798794489091ec12d15a57c9a4bbe677cb9f04
divineros3 <divineros3@icloud.com>
2023-09-14 14:06:06 -0600

    Method Class Submission Assignment

---
+++ MethodClassSubmission/MethodClassSubmission/MathOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    public class MathOperations
+    {
+        public void PerformMathOperation(int number1, int number2)
+        {
+            int result = number1 * 2;
+            Console.WriteLine($"Result of the math operation: {result}");
+            Console.WriteLine($"Second integer: {number2}");
+        }
+    }
+}
---
+++ MethodClassSubmission/MethodClassSubmission/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    class Program
+    {
+        static void Main()
+        {
+            MathOperations mathOperations = new MathOperations();
+
+            Console.Write("Enter the first number: ");
+            if (int.TryParse(Console.ReadLine(), out int number1))
+            {
+                Console.Write("Enter the second number: ");
+                if (int.TryParse(Console.ReadLine(), out int number2))
+                {
+                    mathOperations.PerformMathOperation(number1, number2);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input for the second number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for the first number.");
+            }
+
+            Console.WriteLine("Enter the first number again: ");
+            if (int.TryParse(Console.ReadLine(), out int num1))
+            {
+                Console.Write("Enter the second number again: ");
+                if (int.TryParse(Console.ReadLine(), out int num2))
+                {
+                    mathOperations.PerformMathOperation(number1: num1, number2: num2);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input for the second number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for the first number.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
