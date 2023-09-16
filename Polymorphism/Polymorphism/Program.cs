9de7c23286e0ea75136a3428d05df1cd9f09c904
divineros3 <divineros3@icloud.com>
2023-09-16 14:12:29 -0600

    Polymorphism Submission Assignment

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Employee"
+            };
+
+            employee.SayName();
+
+            IQuittable quittable = employee;
+
+            quittable.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
