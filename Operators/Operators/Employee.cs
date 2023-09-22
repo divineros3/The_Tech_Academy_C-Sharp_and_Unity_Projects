413838d57cf439dab549601a4d19429d769652f3
divineros3 <divineros3@icloud.com>
2023-09-22 17:02:42 -0600

    Parameters Submission Assignment

--- Operators/Operators/Employee.cs
+++ Operators/Operators/Employee.cs
@@ -6,44 +6,45 @@ using System.Threading.Tasks;
 
 namespace Operators
 {
-    public class Employee
+    public class Employee<T>
     {
+        public List<T> Things { get; set; }
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public override bool Equals(object obj)
-        {
-            if (obj is Employee otherEmployee)
-            {
-                return Id == otherEmployee.Id;
-            }
-            return false;
-        }
+        //public override bool Equals(object obj)
+        //{
+        //    if (obj is Employee otherEmployee)
+        //    {
+        //        return Id == otherEmployee.Id;
+        //    }
+        //    return false;
+        //}
 
-        public override int GetHashCode()
-        {
-            return Id.GetHashCode();
-        }
+        //public override int GetHashCode()
+        //{
+        //    return Id.GetHashCode();
+        //}
 
-        public static bool operator ==(Employee employee1, Employee employee2)
-        {
-            if (ReferenceEquals(employee1, employee2))
-            {
-                return true;
-            }
+        //public static bool operator ==(Employee employee1, Employee employee2)
+        //{
+        //    if (ReferenceEquals(employee1, employee2))
+        //    {
+        //        return true;
+        //    }
 
-            if (employee1 is null || employee2 is null)
-            {
-                return false;
-            }
+        //    if (employee1 is null || employee2 is null)
+        //    {
+        //        return false;
+        //    }
 
-            return employee1.Id == employee2.Id;
-        }
+        //    return employee1.Id == employee2.Id;
+        //}
 
-        public static bool operator !=(Employee employee1, Employee employee2)
-        {
-            return !(employee1 == employee2);
-        }
+        //public static bool operator !=(Employee employee1, Employee employee2)
+        //{
+        //    return !(employee1 == employee2);
+        //}
     }
 }
--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -8,8 +8,31 @@ namespace Operators
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Employee<string> stringEmployee = new Employee<string>
+            {
+                Things = new List<string> { "Apple", "Banana", "Cherry" }
+            };
+
+            Employee<int> intEmployee = new Employee<int>
+            {
+                Things = new List<int> { 1, 2, 3, 4, 5 }
+            };
+
+            Console.WriteLine("Things for stringEmployee:");
+            foreach (var item in stringEmployee.Things)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nThings for intEmployee:");
+            foreach (var item in intEmployee.Things)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
