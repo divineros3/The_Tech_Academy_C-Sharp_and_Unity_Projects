cf9a7c460a0a6dca5bd25967a71b16bd460f0f94
divineros3 <divineros3@icloud.com>
2023-09-22 16:35:10 -0600

    Operators Submission update

--- Operators/Operators/Employee.cs
+++ Operators/Operators/Employee.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace Operators
 {
-    class Employee
+    public class Employee
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Employee otherEmployee)
+            {
+                return Id == otherEmployee.Id;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (ReferenceEquals(employee1, employee2))
+            {
+                return true;
+            }
+
+            if (employee1 is null || employee2 is null)
+            {
+                return false;
+            }
+
+            return employee1.Id == employee2.Id;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return !(employee1 == employee2);
+        }
     }
 }
