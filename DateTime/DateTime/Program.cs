9d3d05ed98ca8827cd330fde6185d897b4290c8f
divineros3 <divineros3@icloud.com>
2023-10-11 23:30:05 -0600

    DateTimeSubmissionAssignment

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTimeSubmission
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine("Current Date and Time: " + currentDateTime);
+
+            Console.Write("Enter a number of hours: ");
+            if (int.TryParse(Console.ReadLine(), out int hours))
+            {
+                DateTime futureTime = currentDateTime.AddHours(hours);
+                Console.WriteLine($"Exact time in {hours} hours: {futureTime}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number of hours.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
